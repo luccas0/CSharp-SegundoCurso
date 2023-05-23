fe2dc0215ced8b161c69286fd018126d150e7d21
luccas0 <79585951+luccas0@users.noreply.github.com>
2023-05-23 10:44:49 -0300

    Adicionando consoles de saída

--- PDesafio5-Construtor/Program.cs
+++ PDesafio5-Construtor/Program.cs
@@ -2,3 +2,9 @@
 
 ContaCorrente conta = new ContaCorrente("Luiz", 10, "2020-Q", 200.00);
 
+Console.WriteLine("Titular: " + conta.titular);
+Console.WriteLine("Número da agência: " + conta.numero_agencia);
+Console.WriteLine("Conta: " + conta.conta);
+Console.WriteLine("Saldo: " + conta.saldo);
+
+
